de93646a1c57cd2377d42fc4d7ecffa75b6da998
Cory Fabian <Coryf65@users.noreply.github.com>
2022-12-05 14:25:49 -0600

    created build and added images
    created build and saving screen shots for docs

--- TyperGame/Form1.Designer.cs
+++ TyperGame/Form1.Designer.cs
@@ -125,7 +125,7 @@
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Typer Game";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
